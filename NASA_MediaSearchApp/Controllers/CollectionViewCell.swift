8338e012d5c3db278b18286e270cf9c4ebb5ca07
Zeus <zeusmans@gmail.com>
2022-01-11 12:24:02 -0800

    Fixed image catching and loading here. Also resized cells to all be consistent size.

--- NASA_MediaSearchApp/Controllers/CollectionViewCell.swift
+++ NASA_MediaSearchApp/Controllers/CollectionViewCell.swift
@@ -25,25 +25,68 @@ class CollectionViewCell: UICollectionViewCell {
 // MARK: -                                                                      Prepares cells for reuse
         override func prepareForReuse() {
             super.prepareForReuse()
+            
             imageInCell.image = nil
             imageDetailLabel.text = nil
             
         }
         
+  
+    let imageCache = AutoPurgingImageCache(
+        memoryCapacity: 100_000_000,
+        preferredMemoryUsageAfterPurge: 60_000_000
+    )
+    
+//    let nasaImage = UIImage(data: data)!
+    
 // MARK: -                                                                      Configures cells with image and title
     func configureCells(imageName:String, url: String){
         
         imageDetailLabel.text = imageName
+       // self.imageInCell = nil
+        
+       
+    
+        
+        let urlRequest = URLRequest(url: (URL(string: url)!))
+        
+        
+                
+    
+        
+        if let imageFromCache = imageCache.image(for: urlRequest, withIdentifier: "firstPull") {
+            self.imageInCell?.image = imageFromCache
+            return
+        }
+        
         
         AF.request(url).responseImage { response  in
             if let image = response.value {
                 
+                let size = CGSize(width: 150.0, height: 190.0)
+               // let size = CGSize(width:(self.frame.size.width/3)-3,height: (self.frame.size.width/3)-3)
+                
+              //  image.af.inflate()
+                
                 DispatchQueue.main.async {
                     let radius: CGFloat = 15.0
+                    image.af.inflate()
+                    
+                    self.imageCache.add(image, for: urlRequest, withIdentifier: "firstPull")
                     self.imageInCell?.image = image.af.imageRounded(withCornerRadius: radius)
+                    self.imageInCell?.image = image.af.imageAspectScaled(toFill: size)
+                    
+                    //  self.imageInCell?.image = image.af.imageScaled(to: size)
+                    //  self.imageInCell?.image = image.af.imageAspectScaled(toFit: size)
+//                    let imageToCache = UIImage(data: data)!
+//
+//                    imageCache.add(imageToCache, withIdentifier: "\(url)")
                     
                 }
             }
         }
+        
+        
+    
     }
 }
--- NASA_MediaSearchApp/Controllers/SearchViewController.swift
+++ NASA_MediaSearchApp/Controllers/SearchViewController.swift
@@ -60,19 +60,50 @@ class SearchViewController: UIViewController, UICollectionViewDelegate, UISearch
 
 extension SearchViewController: UICollectionViewDataSource  {
     
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
     
+    
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        return CGSize(
+//            width: (view.frame.size.width/3)-3,
+//        height: (view.frame.size.width/3)-3)
+//    }
+    
+  
+          
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top:1,left:1,bottom:1, right:1)
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+  
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+
         return nasaItems.count
         
     }
-          
+    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier:  "CollectionViewCell", for: indexPath) as! CollectionViewCell
+    
+//Reminder to set collectionView > Estimate Size to "NONE"
         
+//    cell.layoutIfNeeded()
         
         if let imageName = nasaItems[indexPath.item].title, let imageLink = nasaItems[indexPath.item].imageLink {
             
-            cell.configureCells(imageName: imageName, url: imageLink)
+            let urlComponent = URLComponents(string: imageLink)
+            
+            if let url = urlComponent?.url {
+            
+                cell.configureCells(imageName: imageName, url: "\(url)")
+                
+            }
+            
         }
         
         return cell
@@ -82,6 +113,7 @@ extension SearchViewController: UICollectionViewDataSource  {
           
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         
+        print("Selected section \(indexPath.section) and row \(indexPath.row)")
         
         let imageSelectedView:ImageDetailsViewController = self.storyboard?.instantiateViewController(identifier: "showImageDetailsVC") as! ImageDetailsViewController
         
